532c4f1f13c1aa152218a10e4bbf5eb7b2bd00fa
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-14 17:02:38 +0900

    API実装 & 新規作成ページにもAPI通信を実装

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -11,3 +11,18 @@ export async function GET(req: Request, res: Response) {
 
     return Response.json(data);
 }
+
+export async function POST(req: Request, res: Response) {
+    const supabase = await createClient()
+    const { id, title, period } = await req.json();
+
+    const { data, error } = await supabase
+        .from('plans')
+        .insert([{ id, title, period, created_at: new Date().toISOString() }]);
+
+    if (error) {
+        return NextResponse.json(error);
+    }
+
+    return NextResponse.json(data);
+}
--- src/app/plan/new/page.tsx
+++ src/app/plan/new/page.tsx
@@ -1,22 +1,43 @@
 'use client';
+import { useState } from "react";
 
 export default function CreatePlan () {
+    const [id, setId] = useState('');
+    const [title, setTitle] = useState('');
+    const [period, setPeriod] = useState('');
+
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+
+        const newPlan = await fetch(`${API_URL}/api`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ id, title, period }),
+        });
+
+        console.log(newPlan);
+    };
+
     return (
         <div>
             <h2>Create Plan</h2>
     
-            <form>
+            <form onSubmit={ handleSubmit }>
                 <div>
                     <label>URL</label>
-                    <input type="text" />
+                    <input type="text" value={id} onChange={(e) => setId(e.target.value)} />
                 </div>
                 <div>
                     <label>タイトル</label>
-                    <input type="text" />
+                    <input type="text" value={title} onChange={(e) => setTitle(e.target.value)} />
                 </div>
                 <div>
                     <label>期間</label>
-                    <input type="text" />日間
+                    <input type="text" value={period} onChange={(e) => setPeriod(e.target.value)} />日間
                 </div>
                 <button type="submit">
                     作成
