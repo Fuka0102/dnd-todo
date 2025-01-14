41a73217136c28fc4159bac6e1abe39ae5f7f9ad
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-14 16:29:21 +0900

    APIを叩いて、一覧を表示する

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
 import Link from "next/link";
+import { Database } from '../types/database.types';
 
-export default function Home() {
+export default async function Home() {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const res = await fetch(`${API_URL}/api/`, { cache: "no-store" });
+  const plans = await res.json()
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <ul>
-            <li>
-              <Link href={``}>
-                  <div>
-                    <h2>title</h2>
-                  </div>
-              </Link>
-            </li>
+            <ul>
+            {plans.map((plan) => (
+              <li key={plan.id}>
+                <Link href={``}>
+                    <div>
+                      <h2>{plan.title}</h2>
+                      <div>{plan.period}</div>
+                    </div>
+                </Link>
+              </li>
+            ))}
           </ul>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
