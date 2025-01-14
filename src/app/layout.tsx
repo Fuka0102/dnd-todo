eec829f2c42600c514db2486bebae6dbd8086218
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-14 17:30:48 +0900

    ヘッダーロゴにリンク設置

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import Button from './components/Button';
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Dnd Todo",
@@ -18,7 +19,9 @@ export default function RootLayout({
           <header className="bg-yellow-300">
             <div className="max-w-screen-lg my-0 mx-auto py-2.5">
               <div className="flex justify-between items-center">
-                <h1 className="text-xl font-bold">trip todo</h1>
+                <h1 className="text-xl font-bold">
+                  <Link href="/">trip todo</Link>
+                </h1>
           <Button link='/plan/new' text='新規プラン作成' />
               </div>
             </div>
