fa8d1f61d48f63873789bf31549236be2e78728b
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-16 18:23:13 +0900

    新規todo登録処理実装

--- src/app/components/AddTodo.tsx
+++ src/app/components/AddTodo.tsx
@@ -1,8 +1,29 @@
 `use client`;
 
+import React, { useState } from "react";
+
 export default function AddTodo () {
+    const [todoText, setTodoText] = useState(""); 
+
+    function onChangeTodoText (e: React.ChangeEvent<HTMLInputElement>) {
+        setTodoText(e.target.value);
+    }
+
+    function onClickAdd () {
+        if (todoText === "") return;
+        
+        const newTodo = {
+            id: todoText,
+            title: todoText,
+        }
+
+        setTodoText("");
+    }
+
     return (
         <div>
+            <input value={todoText} onChange={onChangeTodoText} />
+            <button onClick={onClickAdd}>追加</button>
         </div>
     )
 }
