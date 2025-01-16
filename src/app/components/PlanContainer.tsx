f496dcf79aa40194c6b53e94a1cd60f34dd8fcc3
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-16 16:29:13 +0900

    衝突検出の処理を実装

--- src/app/components/PlanContainer.tsx
+++ src/app/components/PlanContainer.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, {useState} from 'react';
-import { DndContext, DragEndEvent, useDroppable, Active, Over } from '@dnd-kit/core';
+import { DndContext, DragEndEvent, Active, Over, CollisionDetection, closestCorners, UniqueIdentifier } from '@dnd-kit/core';
 import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 import PlanItem from './PlanItem';
 
@@ -50,7 +50,7 @@ export default function PlanContainer () {
             ],
         }]
     });
-    const [id, setActiveId] = useState(null);
+    const [id, setActiveId] = useState<UniqueIdentifier | null>(null);
 
     function getSortedData(event: { active: Active; over: Over | null }) {
         const { active, over } = event;
@@ -93,7 +93,36 @@ export default function PlanContainer () {
         setData({...data, lists: newLists});
     }
 
+    const customClosestCorners: CollisionDetection = (args) => {
+        const cornerCollisions = closestCorners(args);
+        
+        // 一番近いリストのコンテナを取得
+        const listIds = new Set(data.lists.map(list => list.id));
+        const closestContainer = cornerCollisions.find((c) => {
+            return listIds.has(c.id.toString())
+        });
+
+        if(!closestContainer) return cornerCollisions;
+        
+        // closestContainerの中のチケットのみを取得
+        const collisions = cornerCollisions.filter(({ data }) => {
+            if(!data) return false;
+            const droppableData = data.droppableContainer?.data?.current;
+            if(!droppableData) return false;
+            const { containerId } = droppableData.sortable;
+            return closestContainer.id === containerId;
+        });
+        
+        // 中身のチケットがない場合は、closestContainerを返す
+        if (collisions.length === 0) {
+            return [closestContainer];
+        }
+        // 中身のチケットがある場合は、collisionsを返す
+        return collisions;
+    };
+
     return (
+        <DndContext onDragEnd={handleDragEnd} id={data.id} collisionDetection={customClosestCorners}>
             <div className="flex">
                 {data.lists.map((list) => (
                     <div className="border" key={list.id}>
