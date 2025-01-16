e89c054d13581b237112032158aeeb9df7121d77
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-16 10:31:51 +0900

    サンプルデータ変更

--- src/app/components/PlanContainer.tsx
+++ src/app/components/PlanContainer.tsx
@@ -7,22 +7,48 @@ import PlanItem from './PlanItem';
 
 type Data = {
     id: string,
-    title: string,
-    todos: {
+    name: string,
+    lists: {
         id: string,
-        title: string
+        title: string,
+        todos: {
+            id: string,
+            title: string
+        }[]
     }[]
 }
 
 export default function PlanContainer () {
     const [data, setData] = useState<Data>({
-        id: 'list-sample',
-        title: 'List Sample',
-        todos: [
-            {id: 'todo1', title: 'todo 1'},
-            {id: 'todo2', title: 'todo 2'},
-            {id: 'todo3', title: 'todo 3'},
-        ],
+        id: 'PJ1',
+        name: 'Project 1',
+        lists: [
+            {
+            id: 'list-sample',
+            title: 'List Sample',
+            todos: [
+                {id: 'todo1', title: 'todo 1'},
+                {id: 'todo2', title: 'todo 2'},
+                {id: 'todo3', title: 'todo 3'},
+            ],
+        },
+        {
+            id: 'list-sample2',
+            title: 'List Sample2',
+            todos: [
+                {id: 'todo4', title: 'todo 4'},
+                {id: 'todo5', title: 'todo 5'},
+                {id: 'todo6', title: 'todo 6'},
+            ],
+        },
+        {
+            id: 'list-sample3',
+            title: 'List Sample3',
+            todos: [
+                {id: 'todo7', title: 'todo 7'},
+                {id: 'todo8', title: 'todo 8'},
+            ],
+        }]
     });
 
     function handleDragEnd (event : DragEndEvent) {
