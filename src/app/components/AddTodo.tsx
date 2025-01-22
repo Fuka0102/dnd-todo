29b85d5c9ed241da8dd5bffc5ccc31f35ef277c7
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-01-22 16:37:54 +0900

    style実装

--- src/app/components/AddTodo.tsx
+++ src/app/components/AddTodo.tsx
@@ -10,9 +10,9 @@ type PropsType = {
 
 export default function AddTodo ({ todoText, onChangeTodoText, onClickAdd }: PropsType) {
     return (
-        <div>
-            <input value={todoText} onChange={onChangeTodoText} />
-            <button onClick={onClickAdd}>追加</button>
+        <div className="flex">
+            <input className="block py-1.5 px-0 w-1/5 text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" value={todoText} onChange={onChangeTodoText} placeholder="todo内容を入力" />
+            <button className="flex px-2 py-1.5 bg-black text-white border-solid border-2 border-black hover:bg-white hover:text-black hover:border-black ml-3" onClick={onClickAdd}>追加</button>
         </div>
     )
 }
