a63a1ed8991fc2afaf240d9c38adb95f0b65a140
田中 風花 <Code, planning, and automationSecurityIntegrationsArchivesEmailsmizunofuka0102@gmail.comPrimaryThis email will be used for account-related notifications and can also be used for password resets.Not visible in emailsThis email will not be used as the 'from' address for web-based Git operations, e.g., edits and merges. We will instead use 40429722+Fuka0102@users.noreply.github.com.Receives notificationsThis email address is the default used for GitHub notifications, i.e., replies to issues, pull requests, etc.fuka.tanaka@nijibox.co.jp>
2025-03-14 18:37:37 +0900

    Droppableのpropsに暫定的に型追加

--- src/app/components/Droppable.tsx
+++ src/app/components/Droppable.tsx
@@ -1,7 +1,8 @@
 
 import { useDroppable } from '@dnd-kit/core';
+import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from 'react';
 
-function Droppable (props) {
+function Droppable (props: { id: any; children: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; }) {
     const {setNodeRef} = useDroppable({
         id: props.id,
     });
