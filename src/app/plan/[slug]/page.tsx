a362cf3c8317f36454aa113b820a9a6473a1c9c4
zumi0102 <tanakafuka0102@gmail.com>
2025-06-10 18:51:40 +0900

    フォーマット

--- src/app/plan/[slug]/page.tsx
+++ src/app/plan/[slug]/page.tsx
@@ -1,25 +1,27 @@
-import React from "react";
+/** @format */
+
+import React from 'react';
 import PlanContainer from '../../components/PlanContainer';
 
 export default async function Plan({ params }: { params: Promise<{ slug: string }> }) {
-    const API_URL = process.env.NEXT_PUBLIC_API_URL;
-    const { slug } = await params;
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const { slug } = await params;
 
-    const res = await fetch(`${API_URL}/api/${slug}`, {
+  const res = await fetch(`${API_URL}/api/${slug}`, {
     next: {
-        revalidate: 10,
+      revalidate: 10,
     },
-    });
-    const plan = await res.json();
+  });
+  const plan = await res.json();
 
-    return (
-        <div className="max-w-screen-lg my-0 mx-auto py-2.5">
-            <div className="mt-10">
-                <h2 className="font-bold text-2xl text-center">{plan.title}</h2>
-            </div>
-            <div>
-                <PlanContainer planData={plan} pageId={slug} />
-            </div>
-        </div>
-    );
+  return (
+    <div className='max-w-screen-lg my-0 mx-auto py-2.5'>
+      <div className='mt-10'>
+        <h2 className='font-bold text-2xl text-center'>{plan.title}</h2>
+      </div>
+      <div>
+        <PlanContainer planData={plan} pageId={slug} />
+      </div>
+    </div>
+  );
 }
