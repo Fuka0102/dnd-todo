df0848d8ecbb2adcffca4f70c6a90f88f1ac7ea8
zumi0102 <tanakafuka0102@gmail.com>
2025-06-10 16:58:28 +0900

    フォーマット

--- src/app/components/PlanContainer.tsx
+++ src/app/components/PlanContainer.tsx
@@ -1,260 +1,291 @@
+/** @format */
+
 'use client';
 
-import React, {useState} from 'react';
-import { DndContext, DragEndEvent, Active, Over, CollisionDetection, closestCorners, UniqueIdentifier, DragStartEvent, DragOverEvent, useSensor, useSensors, MouseSensor } from '@dnd-kit/core';
+import React, { useState } from 'react';
+import {
+  DndContext,
+  DragEndEvent,
+  Active,
+  Over,
+  CollisionDetection,
+  closestCorners,
+  UniqueIdentifier,
+  DragStartEvent,
+  DragOverEvent,
+  useSensor,
+  useSensors,
+  MouseSensor,
+} from '@dnd-kit/core';
 import { arrayMove, SortableContext } from '@dnd-kit/sortable';
 import PlanItem from './PlanItem';
 import AddTodo from '../components/AddTodo';
 import Droppable from '../components/Droppable';
-import { FiSave } from "react-icons/fi";
-import { MdOutlineCancel } from "react-icons/md";
+import { FiSave } from 'react-icons/fi';
+import { MdOutlineCancel } from 'react-icons/md';
 
 export type Data = {
-    id: string,
-    name: string,
-    lists: {
-        id: string,
-        title: string,
-        todos: {
-            id: string,
-            title: string
-        }[]
-    }[]
-}
+  id: string;
+  name: string;
+  lists: {
+    id: string;
+    title: string;
+    todos: {
+      id: string;
+      title: string;
+    }[];
+  }[];
+};
 
-export default function PlanContainer ({planData, pageId}) {
-    const [data, setData] = useState<Data>(planData.todos);
-    const [id, setActiveId] = useState<UniqueIdentifier | null>(null);
-    const [editedItemId, setEditedItemId] = useState<string | null>(null);
-    const [editedText, setEditedText] = useState("");
+export default function PlanContainer({ planData, pageId }) {
+  const [data, setData] = useState<Data>(planData.todos);
+  const [id, setActiveId] = useState<UniqueIdentifier | null>(null);
+  const [editedItemId, setEditedItemId] = useState<string | null>(null);
+  const [editedText, setEditedText] = useState('');
 
-    const sensors = useSensors(
-        useSensor(MouseSensor, { activationConstraint: { distance: 5 } })
-    )
+  const sensors = useSensors(useSensor(MouseSensor, { activationConstraint: { distance: 5 } }));
 
-    function getSortedData(event: { active: Active; over: Over | null }) {
-        const { active, over } = event;
-        if (!over) return;
-        if (active.id === over.id) return;
+  function getSortedData(event: { active: Active; over: Over | null }) {
+    const { active, over } = event;
+    if (!over) return;
+    if (active.id === over.id) return;
 
-        const fromSortable = active.data.current?.sortable;
-        if (!fromSortable) return;
+    const fromSortable = active.data.current?.sortable;
+    if (!fromSortable) return;
 
-        const toSortable = over.data.current?.sortable;
-        const notToSortable = {
-            containerId: over.id,
-            index: NaN,
-            items: NaN,
-        }
-
-        return {
-            from: fromSortable,
-            to: toSortable ?? notToSortable,
-        };
-    }
-
-    function handleDragStart(event: DragStartEvent) {
-        const {active} = event;
-        if(!active) return;
-        setActiveId(active.id);
-    }
-
-    function handleDragEnd(event: DragEndEvent) {
-        setActiveId(null);
-        const sortedData = getSortedData(event);
-        if(!sortedData) return;
-
-        const {from, to} = sortedData;
-        if(from.containerId !== to.containerId) return;
-
-        const list = data.lists.find(list => list.id == from.containerId);
-        if(!list) return;
-
-        const newTodos = arrayMove(list.todos, from.index, to.index);
-        const newLists = data.lists.map(list => {
-            if(list.id === from.containerId) return {...list, todos: newTodos};
-            return list;
-        });
-
-        setData({...data, lists: newLists});
-    }
-
-    function handleDragOver(event: DragOverEvent){
-        const sortedData = getSortedData(event);
-
-        if(!sortedData) return;
-
-        const {from, to} = sortedData;
-
-        if(from.containerId === to.containerId) return;
-
-        const fromList = data.lists.find(list => list.id == from.containerId);
-        const toList = data.lists.find(list => list.id == to.containerId);
-        if(!fromList || !toList) return;
-
-        const moveTodo = fromList.todos.find(todo => todo.id === from.items[from.index]);
-        if(!moveTodo) return;
-
-        const newFromTodo = fromList.todos.filter((todo) => todo.id !== moveTodo.id);
-        
-        const newToTodo = [...toList.todos.slice(0, to.index), moveTodo, ...toList.todos.slice(to.index)];
-
-        const newLists = data.lists.map(list => {
-            if(list.id === from.containerId) return {...list, todos: newFromTodo};
-            if(list.id === to.containerId) return {...list, todos: newToTodo};
-
-            return list;
-        });
-        setData({...data, lists: newLists});
-    }
-
-    const customClosestCorners: CollisionDetection = (args) => {
-        const cornerCollisions = closestCorners(args);
-        
-        // 一番近いリストのコンテナを取得
-        const listIds = new Set(data.lists.map(list => list.id));
-        const closestContainer = cornerCollisions.find((c) => {
-            return listIds.has(c.id.toString())
-        });
-
-        if(!closestContainer) return cornerCollisions;
-        
-        // closestContainerの中のチケットのみを取得
-        const collisions = cornerCollisions.filter(({ data }) => {
-            if(!data) return false;
-            const droppableData = data.droppableContainer?.data?.current;
-            if(!droppableData) return false;
-            const { containerId } = droppableData.sortable;
-            return closestContainer.id === containerId;
-        });
-        
-        // 中身のチケットがない場合は、closestContainerを返す
-        if (collisions.length === 0) {
-            return [closestContainer];
-        }
-        // 中身のチケットがある場合は、collisionsを返す
-        return collisions;
+    const toSortable = over.data.current?.sortable;
+    const notToSortable = {
+      containerId: over.id,
+      index: NaN,
+      items: NaN,
     };
 
-    const [todoText, setTodoText] = useState(""); 
+    return {
+      from: fromSortable,
+      to: toSortable ?? notToSortable,
+    };
+  }
 
-    function onChangeTodoText (e: React.ChangeEvent<HTMLInputElement>) {
-        setTodoText(e.target.value);
+  function handleDragStart(event: DragStartEvent) {
+    const { active } = event;
+    if (!active) return;
+    setActiveId(active.id);
+  }
+
+  function handleDragEnd(event: DragEndEvent) {
+    setActiveId(null);
+    const sortedData = getSortedData(event);
+    if (!sortedData) return;
+
+    const { from, to } = sortedData;
+    if (from.containerId !== to.containerId) return;
+
+    const list = data.lists.find((list) => list.id == from.containerId);
+    if (!list) return;
+
+    const newTodos = arrayMove(list.todos, from.index, to.index);
+    const newLists = data.lists.map((list) => {
+      if (list.id === from.containerId) return { ...list, todos: newTodos };
+      return list;
+    });
+
+    setData({ ...data, lists: newLists });
+  }
+
+  function handleDragOver(event: DragOverEvent) {
+    const sortedData = getSortedData(event);
+
+    if (!sortedData) return;
+
+    const { from, to } = sortedData;
+
+    if (from.containerId === to.containerId) return;
+
+    const fromList = data.lists.find((list) => list.id == from.containerId);
+    const toList = data.lists.find((list) => list.id == to.containerId);
+    if (!fromList || !toList) return;
+
+    const moveTodo = fromList.todos.find((todo) => todo.id === from.items[from.index]);
+    if (!moveTodo) return;
+
+    const newFromTodo = fromList.todos.filter((todo) => todo.id !== moveTodo.id);
+
+    const newToTodo = [...toList.todos.slice(0, to.index), moveTodo, ...toList.todos.slice(to.index)];
+
+    const newLists = data.lists.map((list) => {
+      if (list.id === from.containerId) return { ...list, todos: newFromTodo };
+      if (list.id === to.containerId) return { ...list, todos: newToTodo };
+
+      return list;
+    });
+    setData({ ...data, lists: newLists });
+  }
+
+  const customClosestCorners: CollisionDetection = (args) => {
+    const cornerCollisions = closestCorners(args);
+
+    // 一番近いリストのコンテナを取得
+    const listIds = new Set(data.lists.map((list) => list.id));
+    const closestContainer = cornerCollisions.find((c) => {
+      return listIds.has(c.id.toString());
+    });
+
+    if (!closestContainer) return cornerCollisions;
+
+    // closestContainerの中のチケットのみを取得
+    const collisions = cornerCollisions.filter(({ data }) => {
+      if (!data) return false;
+      const droppableData = data.droppableContainer?.data?.current;
+      if (!droppableData) return false;
+      const { containerId } = droppableData.sortable;
+      return closestContainer.id === containerId;
+    });
+
+    // 中身のチケットがない場合は、closestContainerを返す
+    if (collisions.length === 0) {
+      return [closestContainer];
     }
+    // 中身のチケットがある場合は、collisionsを返す
+    return collisions;
+  };
 
-    function onClickAdd () {
-        if (todoText === "") return;
+  const [todoText, setTodoText] = useState('');
 
-        const copiedTodoData = { ...data };
-        
-        const newTodo = {
-            id: todoText,
-            title: todoText,
-        }
+  function onChangeTodoText(e: React.ChangeEvent<HTMLInputElement>) {
+    setTodoText(e.target.value);
+  }
 
-        // TODO: 'list1'と静的に記載しているのがよくないので修正したい
-        const firstList = copiedTodoData.lists.find(list => list.id === 'list1');
+  function onClickAdd() {
+    if (todoText === '') return;
 
-        if(!firstList) return;
+    const copiedTodoData = { ...data };
 
-        copiedTodoData.lists.map((list, index) => {
-            if (index === 0) {
-                list.todos.push(newTodo)
-            }
-        });
-
-        setData(copiedTodoData);
-
-        setTodoText("");
-    }
-
-    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
-        e.preventDefault();
-
-        const API_URL = process.env.NEXT_PUBLIC_API_URL;
-        const todos = data;
-
-        const newPlan = await fetch(`${API_URL}/api/${pageId}`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ id: pageId, todos }),
-        });
+    const newTodo = {
+      id: todoText,
+      title: todoText,
     };
 
-    function onClickDelete (id: string) {
-        const copiedTodoData = { ...data };
-        let filteredData = [];
-        
-        copiedTodoData.lists.map((list) => {
-            filteredData = list.todos.filter((todo) => {
-                return todo.id !== id;
-            })
+    // TODO: 'list1'と静的に記載しているのがよくないので修正したい
+    const firstList = copiedTodoData.lists.find((list) => list.id === 'list1');
 
-            list.todos = filteredData;
-        });
+    if (!firstList) return;
 
-        setData(copiedTodoData);
-    }
+    copiedTodoData.lists.map((list, index) => {
+      if (index === 0) {
+        list.todos.push(newTodo);
+      }
+    });
 
-    function onClickEdit (id: string) {
-        const copiedTodoData = { ...data };
-        
-        copiedTodoData.lists.map((list) => {
-            list.todos.map((todo) => {
-                if (todo.id === id) {
-                    todo.title = editedText ? editedText : todo.title;
-                    setEditedItemId(null);
-                }
-            })
-        });
+    setData(copiedTodoData);
 
-        setData(copiedTodoData);
-    }
+    setTodoText('');
+  }
 
-    function onClickEditCancel () {
-        setEditedItemId(null);
-    }
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
 
-    return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <AddTodo todoText={todoText} onChangeTodoText={onChangeTodoText} onClickAdd={onClickAdd} />
-                <DndContext onDragStart={handleDragStart} onDragOver={handleDragOver} onDragEnd={handleDragEnd} id={data.id} collisionDetection={customClosestCorners} sensors={sensors}>
-                    <div className="grid grid-cols-3 gap-4 mt-4">
-                        {data.lists && data.lists.map((list, index) => (
-                                <SortableContext id={list.id} items={list.todos} key={list.id}>
-                                <Droppable key={list.id}  id={list.id}>
-                                    <div className="text-lg font-bold text-center">Day {index + 1}</div>
-                                    <div className="border min-h-80 mt-2">
-                                        {list.todos.map((todo) => (
-                                            <div key={todo.id}>
-                                                {editedItemId === todo.id ? (
-                                                    <div className="w-full inline-flex items-center gap-x-3 py-2 px-4 cursor-grab text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg">
-                                                        <input
-                                                            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                                                            type="text"
-                                                            key={todo.id}
-                                                            placeholder={todo.title}
-                                                            value={editedText}
-                                                            onChange={(e) => setEditedText(e.target.value)}
-                                                        />
-                                                        <button onClick={() => onClickEdit(todo.id)}><FiSave /></button>
-                                                        <button onClick={onClickEditCancel}><MdOutlineCancel /></button>
-                                                    </div>
-                                                ) : (
-                                                    <PlanItem key={todo.id} id={todo.id} title={todo.title} onClickDelete={()=> onClickDelete(todo.id)} onClickEdit={() => setEditedItemId(todo.id)} />
-                                                )}
-                                            </div>
-                                        ))}
-                                    </div>
-                                </Droppable>
-                            </SortableContext>
-                    ))}
+    const API_URL = process.env.NEXT_PUBLIC_API_URL;
+    const todos = data;
+
+    console.log('onSubmit', todos);
+
+    const newPlan = await fetch(`${API_URL}/api/${pageId}`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ id: pageId, todos }),
+    });
+  };
+
+  function onClickDelete(id: string) {
+    const copiedTodoData = { ...data };
+    let filteredData = [];
+    copiedTodoData.lists.map((list) => {
+      filteredData = list.todos.filter((todo) => {
+        return todo.id !== id;
+      });
+      list.todos = filteredData;
+    });
+
+    setData(copiedTodoData);
+  }
+
+  function onClickEdit(id: string) {
+    const copiedTodoData = { ...data };
+
+    copiedTodoData.lists.map((list) => {
+      list.todos.map((todo) => {
+        if (todo.id === id) {
+          todo.title = editedText ? editedText : todo.title;
+          setEditedItemId(null);
+        }
+      });
+    });
+
+    setData(copiedTodoData);
+  }
+
+  function onClickEditCancel() {
+    setEditedItemId(null);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <AddTodo todoText={todoText} onChangeTodoText={onChangeTodoText} onClickAdd={onClickAdd} />
+        <DndContext
+          onDragStart={handleDragStart}
+          onDragOver={handleDragOver}
+          onDragEnd={handleDragEnd}
+          id={data.id}
+          collisionDetection={customClosestCorners}
+          sensors={sensors}
+        >
+          <div className='grid grid-cols-3 gap-4 mt-4'>
+            {data.lists &&
+              data.lists.map((list, index) => (
+                <SortableContext id={list.id} items={list.todos} key={list.id}>
+                  <Droppable key={list.id} id={list.id}>
+                    <div className='text-lg font-bold text-center'>Day {index + 1}</div>
+                    <div className='border min-h-80 mt-2'>
+                      {list.todos.map((todo) => (
+                        <div key={todo.id}>
+                          {editedItemId === todo.id ? (
+                            <div className='w-full inline-flex items-center gap-x-3 py-2 px-4 cursor-grab text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg'>
+                              <input
+                                className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
+                                type='text'
+                                key={todo.id}
+                                placeholder={todo.title}
+                                value={editedText}
+                                onChange={(e) => setEditedText(e.target.value)}
+                              />
+                              <button onClick={() => onClickEdit(todo.id)}>
+                                <FiSave />
+                              </button>
+                              <button onClick={onClickEditCancel}>
+                                <MdOutlineCancel />
+                              </button>
+                            </div>
+                          ) : (
+                            <PlanItem
+                              key={todo.id}
+                              id={todo.id}
+                              title={todo.title}
+                              onClickDelete={() => onClickDelete(todo.id)}
+                              onClickEdit={() => setEditedItemId(todo.id)}
+                            />
+                          )}
+                        </div>
+                      ))}
                     </div>
-                </DndContext>
-            </form>
-        </>
-    )
+                  </Droppable>
+                </SortableContext>
+              ))}
+          </div>
+        </DndContext>
+      </form>
+    </>
+  );
 }
--- src/app/components/PlanItem.tsx
+++ src/app/components/PlanItem.tsx
@@ -1,33 +1,67 @@
+/** @format */
+
 'use client';
 
 import React, { MouseEventHandler } from 'react';
-import { useSortable } from "@dnd-kit/sortable";
-import { FiEdit2 } from "react-icons/fi";
-import { RiDeleteBinLine } from "react-icons/ri";
+import { useSortable } from '@dnd-kit/sortable';
+import { FiEdit2 } from 'react-icons/fi';
+import { RiDeleteBinLine } from 'react-icons/ri';
 
-export default function PlanItem ({ id, title, onClickDelete, onClickEdit } : { 'id': string, 'title': string, 'onClickDelete': MouseEventHandler<HTMLElement>, 'onClickEdit': MouseEventHandler<HTMLElement>}) {
-    const { attributes, listeners, setNodeRef, transform } =
-    useSortable({ id });
+export default function PlanItem({
+  id,
+  title,
+  onClickDelete,
+  onClickEdit,
+}: {
+  id: string;
+  title: string;
+  onClickDelete: MouseEventHandler<HTMLElement>;
+  onClickEdit: MouseEventHandler<HTMLElement>;
+}) {
+  const { attributes, listeners, setNodeRef, transform } = useSortable({ id });
 
-    const style = transform ? {
-    transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
-    } : undefined;
+  const style = transform
+    ? {
+        transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+      }
+    : undefined;
 
-    return (
-        <div className="w-full inline-flex items-center gap-x-3 py-4 px-4 cursor-grab text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg" ref={setNodeRef} style={style} {...listeners} {...attributes}>
-            <div className="w-9/12">{title}</div>
-            <div className="flex align-center">
-                <button onClick={onClickDelete} className="prime:pen-to-square"><RiDeleteBinLine /></button>
-                <button onClick={onClickEdit} className="ml-2"><FiEdit2 /></button>
-            </div>
-            <svg className="shrink-0 size-4 ms-auto text-gray-400 dark:text-neutral-500" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <circle cx="9" cy="12" r="1"></circle>
-                <circle cx="9" cy="5" r="1"></circle>
-                <circle cx="9" cy="19" r="1"></circle>
-                <circle cx="15" cy="12" r="1"></circle>
-                <circle cx="15" cy="5" r="1"></circle>
-                <circle cx="15" cy="19" r="1"></circle>
-            </svg>
-        </div>
-    );
+  return (
+    <div
+      className='w-full inline-flex items-center gap-x-3 py-4 px-4 cursor-grab text-sm font-medium bg-white border border-gray-200 text-gray-800 -mt-px first:rounded-t-lg first:mt-0 last:rounded-b-lg'
+      ref={setNodeRef}
+      style={style}
+      {...listeners}
+      {...attributes}
+    >
+      <div className='w-9/12'>{title}</div>
+      <div className='flex align-center'>
+        <button onClick={onClickDelete} className='prime:pen-to-square'>
+          <RiDeleteBinLine />
+        </button>
+        <button onClick={onClickEdit} className='ml-2'>
+          <FiEdit2 />
+        </button>
+      </div>
+      <svg
+        className='shrink-0 size-4 ms-auto text-gray-400 dark:text-neutral-500'
+        xmlns='http://www.w3.org/2000/svg'
+        width='24'
+        height='24'
+        viewBox='0 0 24 24'
+        fill='none'
+        stroke='currentColor'
+        strokeWidth='2'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+      >
+        <circle cx='9' cy='12' r='1'></circle>
+        <circle cx='9' cy='5' r='1'></circle>
+        <circle cx='9' cy='19' r='1'></circle>
+        <circle cx='15' cy='12' r='1'></circle>
+        <circle cx='15' cy='5' r='1'></circle>
+        <circle cx='15' cy='19' r='1'></circle>
+      </svg>
+    </div>
+  );
 }
