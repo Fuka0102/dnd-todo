d3f3c2c727be823e455ca30d09da3b001b90c562
田中 風花 <tanaka_fuka@waku-2.com>
2025-08-07 16:27:52 +0900

    SaveResultコンポーネントへ切り出し

--- src/app/components/PlanContainer.tsx
+++ src/app/components/PlanContainer.tsx
@@ -350,27 +350,7 @@ export default function PlanContainer({ planData, pageId }: PlanContainerProps) 
             <DeleteContainerButton onDeleteList={onClickDeleteContainer} />
           </div>
         </div>
-        {isStartToSave && (
-            <div className='flex items-center justify-center mt-4'>
-              <div className="text-blue-600 text-center font-bold">
-                  <p>Saving...</p>
-              </div>
-            </div>
-        )}
-          {!isStartToSave && isCompleted && (
-              <div className='flex items-center justify-center mt-4'>
-                <div className="text-green-600 text-center font-bold">
-                    <p>Save successful!</p>
-                </div>
-              </div>
-          )}
-          {!isStartToSave && isFailed && (
-              <div className='flex items-center justify-center mt-4'>
-                <div className="text-red-600 text-center font-bold">
-                    <p>Save failed. Please try again.</p>
-                </div>
-              </div>
-          )}
+        <SaveResult isStartToSave={isStartToSave} isCompleted={isCompleted} isFailed={isFailed} />
         <DndContext
           onDragStart={handleDragStart}
           onDragOver={handleDragOver}
