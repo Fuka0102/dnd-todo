534905fff2d0c20926ab058cc1be4213b158e28e
田中 風花 <tanaka_fuka@waku-2.com>
2025-08-21 18:40:15 +0900

    不要処理削除

--- src/app/components/PlanContainer.tsx
+++ src/app/components/PlanContainer.tsx
@@ -148,34 +148,6 @@ export default function PlanContainer({ planData, pageId }: PlanContainerProps) 
     });
   }
 
-  const customClosestCorners: CollisionDetection = (args) => {
-    const cornerCollisions = closestCorners(args);
-
-    // 一番近いリストのコンテナを取得
-    const listIds = new Set(data.lists.map((list) => list.id));
-    const closestContainer = cornerCollisions.find((c) => {
-      return listIds.has(c.id.toString());
-    });
-
-    if (!closestContainer) return cornerCollisions;
-
-    // closestContainerの中のチケットのみを取得
-    const collisions = cornerCollisions.filter(({ data }) => {
-      if (!data) return false;
-      const droppableData = data.droppableContainer?.data?.current;
-      if (!droppableData) return false;
-      const { containerId } = droppableData.sortable;
-      return closestContainer.id === containerId;
-    });
-
-    // 中身のチケットがない場合は、closestContainerを返す
-    if (collisions.length === 0) {
-      return [closestContainer];
-    }
-    // 中身のチケットがある場合は、collisionsを返す
-    return collisions;
-  };
-
   const [todoText, setTodoText] = useState('');
 
   function onChangeTodoText(e: React.ChangeEvent<HTMLInputElement>) {
