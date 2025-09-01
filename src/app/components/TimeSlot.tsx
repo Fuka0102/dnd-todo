ea40fb3a7fad261c4d57fc1b1e562e84f29b3a0b
田中 風花 <tanaka_fuka@waku-2.com>
2025-09-01 18:37:54 +0900

    TimeSlotに分岐追加

--- src/app/components/TimeSlot.tsx
+++ src/app/components/TimeSlot.tsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { useDroppable } from '@dnd-kit/core';
 
-export default function TimeSlot({ id, listId, time, children } : {
+export default function TimeSlot({ id, listId, time, children, isEditing, onSlotClick, onCreateTodo} : {
   id: string;
   listId: string;
   time: string;
@@ -17,6 +17,22 @@ export default function TimeSlot({ id, listId, time, children } : {
       accepts: ['todo-item'], // 例えば「TODOアイテムのみ受け入れる」といった情報も追加できる
     }
   });
+    // 1. もしTODOが存在すれば、TODOアイテムを表示
+  if (todo) {
+    return (
+      <div className="time-slot">
+      </div>
+    );
+  }
+
+  // 2. もし編集中なら、新規作成フォームを表示
+  if (isEditing) {
+    return (
+      <div className="time-slot">
+      </div>
+    );
+  }
+
   return (
     <div ref={setNodeRef} style={{ height: '50px', border: '1px dashed gray' }}>
       {children}
